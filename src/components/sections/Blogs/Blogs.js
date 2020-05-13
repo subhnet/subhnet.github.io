a5fb3886a7828acdca5e1fbf966d6aba17c3d3fd
smaharan <smaharan@adobe.com>
2020-05-13 19:27:58 +0530

    Update

--- src/components/sections/Blogs/Blogs.js
+++ src/components/sections/Blogs/Blogs.js
@@ -12,19 +12,18 @@ const Blogs = props => {
 
                 <div className="timeline talks-timeline">
                     <ul>
-
                         <li className="timeline_element talk">
                             <div className="timeline_element-date">
-                                <time className="timeline_element-date-text">09 Feb 2019</time>
+                                <time className="timeline_element-date-text">26 Apr 2020</time>
                             </div>
 
                             <div className="timeline_element-contents">
                                 <h3 className="talk-title">
-                                    <b>Developing Chat App with Angular UI and Back-end in Golang</b>
+                                    <b>Realtime Chat app using Kafka, SpringBoot, ReactJS, and WebSockets</b>
                                 </h3>
 
                                 <div className="talk-buttons">
-                                    <a target="_blank" href="https://medium.com/@subhransumaharana_40338/developing-chat-app-with-angular-ui-and-back-end-in-golang-be01de4cdef7"
+                                    <a target="_blank" href="https://dev.to/subhransu/realtime-chat-app-using-kafka-springboot-reactjs-and-websockets-lc"
                                         className="button button-red">
                                         <i className="fa fa-file-text-o"></i>
                                         Read article
@@ -34,7 +33,6 @@ const Blogs = props => {
 
                             </div>
                         </li>
-
                         <li className="timeline_element talk">
                             <div className="timeline_element-date">
                                 <time className="timeline_element-date-text">01 Mar 2020</time>
@@ -58,16 +56,16 @@ const Blogs = props => {
                         </li>
                         <li className="timeline_element talk">
                             <div className="timeline_element-date">
-                                <time className="timeline_element-date-text">26 Apr 2020</time>
+                                <time className="timeline_element-date-text">09 Feb 2019</time>
                             </div>
 
                             <div className="timeline_element-contents">
                                 <h3 className="talk-title">
-                                    <b>Realtime Chat app using Kafka, SpringBoot, ReactJS, and WebSockets</b>
+                                    <b>Developing Chat App with Angular UI and Back-end in Golang</b>
                                 </h3>
 
                                 <div className="talk-buttons">
-                                    <a target="_blank" href="https://dev.to/subhransu/realtime-chat-app-using-kafka-springboot-reactjs-and-websockets-lc"
+                                    <a target="_blank" href="https://medium.com/@subhransumaharana_40338/developing-chat-app-with-angular-ui-and-back-end-in-golang-be01de4cdef7"
                                         className="button button-red">
                                         <i className="fa fa-file-text-o"></i>
                                         Read article
@@ -77,6 +75,7 @@ const Blogs = props => {
 
                             </div>
                         </li>
+
                     </ul>
                 </div>
             </div>
