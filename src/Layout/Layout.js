c15fa80af85ecde5ef852cd7e605f5929d1a6c03
smaharan <smaharan@adobe.com>
2020-05-14 12:50:34 +0530

    Update

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -4,6 +4,7 @@ import Auxilary from './Auxilary';
 import Footer from './../containers/Footer';
 import Banner from './../components/sections/Banner/Banner';
 import ResponsiveNav from './../containers/ResponsiveNav';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 
 const Layout = (props) => {
@@ -19,6 +20,21 @@ const Layout = (props) => {
                         <Footer />
                     </div>
                 </div>
+                <Link
+                    activeClass="active"
+                    to="top"
+                    className="sm-up-container" 
+                    style={{display: "block"}}
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    {/* <a href={props.link}> */}
+                    <i className="fa fa-angle-up sm-up"></i>
+                    {/* </a> */}
+                </Link>
+                {/* <a href="#page" className="sm-up-container" style={{ display: "block" }}><i className="fa fa-angle-up sm-up"></i></a> */}
             </div>
         </Auxilary>
     );
