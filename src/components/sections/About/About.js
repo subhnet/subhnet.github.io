d74fc9e1611ef72f3edd5d186ccfb0ea06485376
Subhransu <smaharan@adobe.com>
2021-07-21 16:03:39 +0530

    Update

--- src/components/sections/About/About.js
+++ src/components/sections/About/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import SkillCard from './card/SkillCard'
 
 const About = props => {
@@ -14,20 +13,14 @@ const About = props => {
                         JPA.</li>
                     <li><b>Front-end: </b><span className="blue">React (with Hooks)</span>, <span className="blue">Angular 4</span>,Jquery,JavaScript, Typescript</li>
                     <li><b>Back-end: </b><span className="blue">Java/J2EE</span> Technologies,<span className="blue">Spring</span>, SpringBoot, NodeJS, ExpressJS , GraphQL</li>
-                    <li>Experience working with Containerization Technologies like <b>Docker</b>, <b>Docker Compose</b></li>
                     <li>Knowledge in cloud based technologies: <span className="blue">AWS</span> and <span className="blue">Azure</span></li>
-                    <li>Knowledge in <span className="blue"><a href="https://golang.org/">Go Programming
-                        Language</a></span></li>
-                    <li>Have extensively used a large number of software engineering tools, including
-                        Maven,
-                        Ant, TFS,git and svn.</li>
-                    
-                    <li>I'm eager to use new technologies for the development of applications.
-                    </li>
+                    <li>Knowledge in <span className="blue"><a href="https://golang.org/">Go Programming Language</a></span></li>
+                    <li>Have extensively used a large number of software engineering tools,including Maven,Ant,TFS,git and svn.</li>
+                    <li>I'm eager to use new technologies for the development of applications.</li>
                     <li>Having Sound knowledge in OOPs ,Data Structures, and design patterns.</li>
                     <li>Proficient with Front End Framework (ReactJS, Angular)</li>
-                    <li>Proficient with Docker and docker-compose</li>
-                    <li>Proficient in building fully automated CI/CD pipelines for code deployment using Github Actions.</li>
+                    <li>Experience with Containerization Technologies like <b>Docker</b>, <b>Docker Compose</b></li>
+                    <li>Experience with building fully automated CI/CD pipelines for code deployment using Github Actions.</li>
                     <li>Experience with Web Servers and Reverse Proxy Tools such as Nginx</li>
                     <li>Experience with Relational Databases (MySQL) and NoSQL databases (DynamoDB)</li>
                     <li>Good Analytics skills and a quick Learner.</li>
