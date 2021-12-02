7403a4cb12e31960ff691974722c485f898517b0
Subhransu <smaharan@adobe.com>
2021-12-02 21:18:05 +0530

    Updates

--- deploy.js
+++
@@ -1,8 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', {
-    branch: 'master',
-    repo: 'https://github.com/subhnet/subhnet.github.io.git'
-}, function (err) {
-    console.log(err);
-});
