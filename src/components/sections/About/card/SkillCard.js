f4ea1458f05865053e258c4f7b47b2694c3d16f2
Subhransu Kumar Maharana <smaharan@adobe.com>
2024-07-02 02:11:19 +0530

    updates

--- src/components/sections/About/card/SkillCard.js
+++ src/components/sections/About/card/SkillCard.js
@@ -1,71 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './SkillCard.css'
+import './SkillCard.css';
 
 const SkillCard = props => {
-    const [skills, setSkills] = useState([
-        { name: "React", score: 4 },
-        { name: 'Java', score: 5 },
-        { name: 'NodeJS', score: 4 },
-        { name: 'JavaScript', score: 3 },
-        { name: 'Spring', score: 4 },
-        { name: 'Angular', score: 3 },
-        { name: 'Docker', score: 3 },
-        { name: 'Python', score: 2}
-    ])
+    const [skills, setSkills] = useState({
+        "Languages": [
+            { name: "JavaScript", score: 5 },
+            { name: "Java", score: 5 },
+            { name: "Python", score: 4 },
+            { name: "TypeScript", score: 4 }
+        ],
+        "Front-End": [
+            { name: "React", score: 4 },
+            { name: "Angular", score: 3 },
+            { name: "JavaScript", score: 4 },
+            { name: "TypeScript", score: 4 },
+            { name: "HTML", score: 5 },
+            { name: "CSS", score: 5 }
+        ],
+        "Back-End": [
+            { name: "Java", score: 5 },
+            { name: "NodeJS", score: 4 },
+            { name: "Spring", score: 4 },
+            { name: "Python", score: 3 },
+            { name: "Express.js", score: 4 },
+            { name: "MySQL", score: 4 }
+        ],
+        "DevOps": [
+            { name: "Docker", score: 3 },
+            { name: "AWS", score: 4 },
+            { name: "Azure", score: 3 },
+            { name: "Apache Airflow", score: 3 },
+            { name: "GitHub Actions", score: 4 }
+        ],
+        "Big Data": [
+            { name: "Hadoop", score: 3 },
+            { name: "PySpark", score: 3 },
+            { name: "AWS Glue", score: 3 },
+            { name: "Kafka", score: 3 }
+        ],
+        "Miscellaneous": [
+            { name: "GraphQL", score: 3 },
+            { name: "Apache OpenWhisk", score: 3 },
+            { name: "Gulp", score: 3 }
+        ]
+    });
 
     return (
-        <div>
-            <div className="EngineerCard__Container-sc-1wpqjm4-0 sqvXk">
-
-                <div class="EngineerCard__Info-sc-1wpqjm4-3 dIcgtl">
-                    <h3 color="darkBlue" class="skillHeading">Skills</h3>
-                    <div class="EngineerCard__SubHeading-sc-1wpqjm4-6 cEMOqc"><div class="sc-bwzfXH fxkJNP sc-bxivhb bUZGRr" color="darkBlue">
-                    </div>
-                    </div>
-                </div>
-                <hr style={{ margin: "14px 0" }} />
-                <div className="EngineerCard__Stats-sc-1wpqjm4-8 dcvOHH">
-                    {/* <div className="EngineerCard__Title-sc-1wpqjm4-9 eyHvrW">
-                        <div className="EngineerCard__OverallScoreWrapper-sc-1wpqjm4-13 cArByi">
-                            <p className="sc-dnqmqq hmVOQX" color="plumBlue">/10</p>
+        <div className="SkillCard">
+            <h3 className="skillHeading">Skills</h3>
+            <div className="skillCategories">
+                {Object.keys(skills).map(category => (
+                    <div key={category} className="category">
+                        <h4>{category}</h4>
+                        <div className="skills">
+                            {skills[category].map(skill => (
+                                <p key={skill.name} className="skill">{skill.name}</p>
+                            ))}
                         </div>
-                        <div className="EngineerCard__Divider-sc-1wpqjm4-4 dLUSXs"></div>
-                        <div className="EngineerCard__RoleContainer-sc-1wpqjm4-10 esWYYf">
-                            <p className="sc-dnqmqq EngineerCard__Role-sc-1wpqjm4-11 bzSKdf" color="plumBlue">Mid-to-Senior</p>
-                        </div>
-                    </div> */}
-                    {
-                        skills.map(skill => {
-                            return (<div className="EngineerCard__Skill-sc-1wpqjm4-12 fnlfPX">
-                                <p color="darkBlue" className="skills">{skill.name}</p>
-                                <div className="EngineerCard__Score-sc-1wpqjm4-15 gffUbb">
-                                    {
-                                        [...Array(skill.score)].map(e => {
-                                            return <div color="#E51700" className="score-dot"></div>
-                                        })
-
-                                    }
-                                    {
-                                        [...Array(5 - skill.score)].map(e => {
-                                            return <div color="#E51700" className="score-dot-empty"></div>
-                                        })
-                                    }
-                                    <div className="feaLkc"></div>
-                                </div>
-                            </div>)
-                        })
-                    }
-
-                </div>
+                    </div>
+                ))}
             </div>
-
         </div>
-    )
+    );
 }
 
-SkillCard.propTypes = {
+SkillCard.propTypes = {};
 
-}
-
-export default SkillCard
+export default SkillCard;
